6be2945f7ea00864b9b6126f220c28d2d85051ae
Valentyn Kolesnikov <javadev75@gmail.com>
2026-08-14 07:58:21 +0300

    Fix formatting issues in Solution.java

--- src/main/java/g3801_3900/s3892_minimum_operations_to_achieve_at_least_k_peaks/Solution.java
+++ src/main/java/g3801_3900/s3892_minimum_operations_to_achieve_at_least_k_peaks/Solution.java
@@ -16,7 +16,6 @@ public class Solution {
         if (k > n / 2) {
             return -1;
         }
-
         long[] c = new long[n];
         for (int i = 0; i < n; i++) {
             int left = nums[(i - 1 + n) % n];
