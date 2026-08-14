16b91826d2c930be4484bd76b9363d0e1311534d
Valentyn Kolesnikov <javadev75@gmail.com>
2026-08-14 08:02:48 +0300

    Improved 3883

--- src/main/java/g3801_3900/s3883_count_non_decreasing_arrays_with_given_digit_sums/Solution.java
+++ src/main/java/g3801_3900/s3883_count_non_decreasing_arrays_with_given_digit_sums/Solution.java
@@ -6,6 +6,7 @@ package g3801_3900.s3883_count_non_decreasing_arrays_with_given_digit_sums;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+@SuppressWarnings("unchecked")
 public class Solution {
     private static final int M = 1000000007;
 
@@ -20,34 +21,27 @@ public class Solution {
 
     public int countArrays(int[] d) {
         ArrayList<Integer>[] g = buildGroups();
-
         if (g[d[0]].isEmpty()) {
             return 0;
         }
-
         long[] dp = createInitialDp(g[d[0]].size());
-
         for (int i = 1; i < d.length; i++) {
             dp = transition(dp, g[d[i - 1]], g[d[i]]);
             if (dp.length == 0) {
                 return 0;
             }
         }
-
         return sum(dp);
     }
 
     private ArrayList<Integer>[] buildGroups() {
         ArrayList<Integer>[] g = new ArrayList[51];
-
         for (int i = 0; i <= 50; i++) {
             g[i] = new ArrayList<>();
         }
-
         for (int i = 0; i <= 5000; i++) {
             g[s(i)].add(i);
         }
-
         return g;
     }
 
@@ -61,42 +55,34 @@ public class Solution {
         if (current.isEmpty()) {
             return new long[0];
         }
-
         long[] prefix = buildPrefixSums(dp);
         long[] next = new long[current.size()];
-
         int k = 0;
         for (int j = 0; j < current.size(); j++) {
             while (k < previous.size() && previous.get(k) <= current.get(j)) {
                 k++;
             }
-
             if (k > 0) {
                 next[j] = prefix[k - 1];
             }
         }
-
         return next;
     }
 
     private long[] buildPrefixSums(long[] values) {
         long[] prefix = new long[values.length];
         prefix[0] = values[0];
-
         for (int i = 1; i < values.length; i++) {
             prefix[i] = (prefix[i - 1] + values[i]) % M;
         }
-
         return prefix;
     }
 
     private int sum(long[] values) {
         long result = 0;
-
         for (long value : values) {
             result = (result + value) % M;
         }
-
         return (int) result;
     }
 }
